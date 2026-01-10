2566065880c70a8a43f44d1d1e0139ab3512077c
HealingAura32 <vqudus@gmail.com>
2026-01-10 15:36:48 -0500

    Add profile model, signals, and initial migrations

--- hoosstudying/settings.py
+++ hoosstudying/settings.py
@@ -31,6 +31,12 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+
+    # My apps
+    'accounts.apps.AccountsConfig',
+    'groups',
+    'board',
+    # Auto generated
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
