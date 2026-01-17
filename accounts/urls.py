30c9d07c287a4978a97736839ac111784f4c1fd4
HealingAura32 <vqudus@gmail.com>
2026-01-17 17:23:07 -0500

    added base.html, added logout feature

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,11 @@
-from django.urls import path
-import django.contrib.auth.urls
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
 from .views import edit_profile, personal_profile_detail, other_profile_detail
 
 app_name = "accounts"
 
 urlpatterns = [
+    path("accounts/", include("django.contrib.auth.urls")),
     path('profile/me/', personal_profile_detail, name='my_profile_view'),
     path('profile/me/edit/', edit_profile, name='edit_profile'),
     path('profile/<str:username>/', other_profile_detail, name='user_profile_view'),
