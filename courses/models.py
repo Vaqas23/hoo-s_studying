5a0f138543e6a09eccfd84cd17b2627f84a8c02b
HealingAura32 <vqudus@gmail.com>
2026-01-19 16:54:46 -0500

    added student and course models and migrated databases

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 # Create your models here.
+
+
+class Course(models.Model):
+    code = models.CharField(max_length=20, unique=True)
+    title = models.CharField(max_length=50, blank=True)
+
+    def __str__(self):
+        return self.code
+
+
+class Student(models.Model):
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, related_name="student")
+    email_verified = models.BooleanField(default=False)
+    courses = models.ManyToManyField(
+        Course, blank=True, related_name="students")
+
+    def __str__(self):
+        return self.user.username
