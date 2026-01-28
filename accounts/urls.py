08be77aef28ece147bcd81a8e29475cad5f67e38
HealingAura32 <vqudus@gmail.com>
2026-01-28 15:26:46 -0500

    Added password reset routing for initial stage of reset

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path("login/", auth_views.LoginView.as_view(template_name="accounts/login.html"), name="login"),
     path("logout/", auth_views.LogoutView.as_view(next_page='accounts:login'), name="logout"),
     path("signup/", views.signup, name="signup"),
+    path('password_reset/', auth_views.PasswordResetView.as_view(
+        template_name="accounts/password_reset_form.html", name="password_reset")),
+    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(
+        template_name="accounts/password_reset_done.html", name="password_reset_done")),
 ]
