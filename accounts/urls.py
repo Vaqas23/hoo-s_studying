7d7fd6b3ff938d058ff0eafc58e723897be2c6e6
HealingAura32 <vqudus@gmail.com>
2026-01-29 14:01:14 -0500

    proper namespacing for accounts and courses urls added to their respective urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, reverse_lazy
 from django.contrib.auth import views as auth_views
 from . import views
 
+app_name = "accounts"
 
 urlpatterns = [
     path('home/', views.homeview, name="home"),
--- courses/urls.py
+++ courses/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+app_name = "courses"
 urlpatterns = [
     path('my_courses/', views.my_courses, name="my_courses")
 ]
